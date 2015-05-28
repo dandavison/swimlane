ca76e08ae9ef8db7f37ddca448bf7840f668f9ed
Dan Davison <dandavison7@gmail.com>
2015-05-28 01:53:18 -0700

    Draw messages

--- swimlane/swimlane.py
+++ swimlane/swimlane.py
@@ -12,6 +12,7 @@ class Swimlane(Drawing):
     peer_rect_width = 200
     peer_rect_height = 800
     peer_rect_gap = 100
+    message_gap = 100
 
     def __init__(self, parsed, *args, **kwargs):
         super(Swimlane, self).__init__(*args, **kwargs)
@@ -19,21 +20,37 @@ class Swimlane(Drawing):
         self.peers = dict.fromkeys(parsed['peers'])
         self.messages = parsed['messages']
 
-    def make_peer_rect(self, origin):
+    def make_peer_rect(self, offset):
         return self.rect(
-            origin,
+            offset,
             (self.peer_rect_width, self.peer_rect_height),
             stroke='black',
             fill='white',
         )
 
+    def make_message_arrow(self, source, target, message, vertical_offset):
+        def get_midline(rect):
+            return rect['x'] + rect['width'] / 2.0
+
+        return self.line(
+            (get_midline(self.peers[source]), vertical_offset),
+            (get_midline(self.peers[target]), vertical_offset),
+            stroke='black',
+        )
+
     def render(self):
 
-        origin = [0, 0]
+        offset = [0, 0]
         for peer in self.peers:
-            self.peers[peer] = self.make_peer_rect(origin)
+            self.peers[peer] = self.make_peer_rect(offset)
             self.add(self.peers[peer])
-            origin[0] += self.peer_rect_width + self.peer_rect_gap
+            offset[0] += self.peer_rect_width + self.peer_rect_gap
+
+        vertical_offset = self.message_gap
+        for message in self.messages:
+            self.add(self.make_message_arrow(*message,
+                                             vertical_offset=vertical_offset))
+            vertical_offset += self.message_gap
 
         return self
 
